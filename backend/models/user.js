5bd6ee6db522d1db09a350aed7ff389ec419e08d
Aditi <aditidubey3737@gmail.com>
2026-02-16 11:06:33 +0530

    Refactor user model: renamed 'username' to 'name', added 'profilePicture' field, and updated password comparison method to 'matchPassword'.

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,17 +3,15 @@ const bcrypt = require('bcrypt');
 const { time } = require('node:console');
 
 const userSchema = new mongoose.Schema({
-    username: {
+    name: {
         type: String,
         required: true,
-        unique: true,
-        minlength: 4
+        minlength: 2
     },
     password: {
         type: String,
         required: true,
         minlength: 6
-
     },
     email: {
         type: String,
@@ -22,24 +20,22 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         unique: true
     },
-    number: {
+    profilePicture: {
         type: String,
-        match: /^\d{10}$/,
-        required: true
+        default: null
     },
 },
 { timestamps: true }
 );
 
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
     if (this.isModified('password')) {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
     }
-    next();
 });
 
-userSchema.methods.comparePassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function(enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
